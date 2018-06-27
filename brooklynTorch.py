cc21b7a42b3d85701d3a01eeefd2971176b7be25
Sergiusz Bleja <sergiusz@bleja.org>
2018-06-27 09:55:51 -0400

    Combined torch and keras in one file

--- brooklynTorch.py
+++ brooklynTorch.py
@@ -15,6 +15,10 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import ShuffleSplit
 
+from keras import Sequential
+from keras.layers import Embedding
+from keras.layers import Dense, Dropout, Activation, Flatten
+
 import pandas as pd
 import numpy as np
 
@@ -26,89 +30,22 @@ import seaborn as sns
 
 from pandas.api.types import CategoricalDtype
 
+from mpl_toolkits.mplot3d import Axes3D
+
 def main():
-
     df = pd.read_csv("~/.kaggle/datasets/new-york-city/nyc-east-river-bicycle-crossings/nyc-east-river-bicycle-counts.csv")
-    df['date'] = pd.to_datetime(df['Date'])
-    df['weekday'] = df['date'].dt.weekday
-    df['weekday_name'] = df['date'].dt.weekday_name
-
-    plotWeekdayCounts(df)
-    weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    weekday_type = CategoricalDtype(categories=weekdays, ordered=True)
-    df['weekday_name'] = df['weekday_name'].astype(weekday_type)
-    plotWeekdayCounts(df)
-    brigde = 'Brooklyn Bridge'
-    plotWeekdayCounts(df,brigde)
-
-    ggplot(df, aes(brigde)) + geom_density(aes(fill = 'weekday_name'), alpha = 0.3) + facet_wrap('~weekday_name')
-
 
     df['users'] = df['Brooklyn Bridge']
-
     df = df[df['users'] > 0]
     df['scaled_users'] = (df['users'] - np.mean(df['users']))/np.std(df['users'])
 
     emb_size = 3
     embedding_names = [f'D{x+1}' for x in np.arange(emb_size)]
 
-    val_idx = get_cv_idxs(len(df))
-
-    n_days = 7
-    df
-
-    data = ColumnarModelData.from_data_frame('', val_idx, df[['weekday']], df['scaled_users'], ['weekday'], 16)
-    
-    def get_emb(num_cat, num_emb):
-        e = nn.Embedding(num_cat, num_emb)
-        e.weight.data.uniform_(-0.01,0.01)
-        return(e)
-
-    class weekdayEmbedding(nn.Module):
-        def __init__(self, n_days):
-            super().__init__()
-            self.weekdays = get_emb(n_days, emb_size)
-            self.lin1 = nn.Linear(emb_size, 30)
-            self.lin2 = nn.Linear(30,1)
-            #self.drop1 = nn.Dropout(0.5)
-        
-        def forward(self, cats, conts):
-            weekdays = cats[:,0]
-            x = self.weekdays(weekdays)
-            x = F.relu((self.lin1(x)))
-            return(self.lin2(x))
-
-    model = weekdayEmbedding(n_days).cuda()
-    opt = optim.Adam(model.parameters(), 1e-3)
-    fit(model, data, 3, opt, F.mse_loss)
-    fit(model, data, 30, opt, F.mse_loss)
-    fit(model, data, 30, opt, F.mse_loss)
-
-    model.weekdays.weight.data
-
-
-    list(model.parameters())
-
-
-    model = keras.Sequential()
-    model.add(Embedding(input_dim=7, output_dim=emb_size, input_length=1, name="embedding"))
-    model.add(Flatten())
-    model.add(Dense(units=40, activation='relu'))
-    model.add(Dense(units=10, activation='relu'))
-    model.add(Dense(units=1))
-
-    model.compile(loss='mse', optimizer='sgd', metrics=['accuracy'])
-
-    hh = model.fit(x=df[['weekday']], y=df[['scaled_users']], epochs=50,batch_size=2)
-
-    mm = model.get_layer('embedding')
-    emb_matrix = mm.get_weights()[0]
-
-    emp_df = pd.DataFrame(emb_matrix, columns = embedding_names)
-    emp_df['weekday'] = np.arange(0,7)
+    torch_emb = calculateTorchEmbeddingMatrix(emb_size, embedding_names, df, batch_size=2)
+    emp_df = calculateKerasEmbeddingMatrix(emb_size, embedding_names, df, batch_size=2)
 
     df = pd.merge(df, emp_df, on = 'weekday')
-
     dummyw = pd.get_dummies(df['weekday_name'])
 
     df_X = pd.concat([df, dummyw], ignore_index= False, axis = 1)
@@ -118,6 +55,7 @@ def main():
     df_X = df_X[all_x]
 
     model = LinearRegression()
+
     cat_x = df_X[weekdays]
     emb_x = df_X[embedding_names]
 
@@ -130,28 +68,110 @@ def main():
 
     ggplot(scores, aes('categorical')) + geom_density() + geom_density(aes('embedded'), color = 'red')
 
+def deriveDateData(df):
+    df['date'] = pd.to_datetime(df['Date'])
+    df['weekday'] = df['date'].dt.weekday
+    df['weekday_name'] = df['date'].dt.weekday_name
+
+    # make the weekdays ordered categories 
+    weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    weekday_type = CategoricalDtype(categories=weekdays, ordered=True)
+    df['weekday_name'] = df['weekday_name'].astype(weekday_type)
+    return(df)
+
+
+def correlationMatrixFor(emp_df):
     corr_mat = emp_df.drop('weekday', axis = 1).transpose().corr()
     corr_mat.columns = weekdays 
     corr_mat['weekdays_name'] = weekdays
     corr_mat.set_index('weekdays_name', inplace = True)
-
-    plt.matshow(corr_mat)
-    plt.colorbar()
-
+    return(corr_mat)
+    
+def seabornHeatmap(corr_mat):
     sns.heatmap(corr_mat)
     sns.heatmap(emp_df.drop('weekday', axis = 1))
 
-    from mpl_toolkits.mplot3d import Axes3D
 
+def chartEmbedding3D(emp_df):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection = '3d')
     ax.scatter(xs = emp_df['D1'], ys = emp_df['D2'], zs = emp_df['D3'], c = emp_df['weekday'])
 
     for row_num, day in emp_df.iterrows():
         ax.text(x = day['D1'] , y = day['D2'], z = day['D3'], s = weekdays[row_num])
+
+
+def calculateKerasEmbeddingMatrix(emb_size, embedding_names, df, batch_size=2):
+    model = Sequential()
+    model.add(Embedding(input_dim=7, output_dim=emb_size, input_length=1, name="embedding"))
+    model.add(Flatten())
+    model.add(Dense(units=40, activation='relu'))
+    model.add(Dense(units=10, activation='relu'))
+    model.add(Dense(units=1))
+
+    model.compile(loss='mse', optimizer='sgd', metrics=['accuracy'])
+
+    hh = model.fit(x=df[['weekday']], y=df[['scaled_users']], epochs=50,batch_size=batch_size)
+
+    mm = model.get_layer('embedding')
+    emb_matrix = mm.get_weights()[0]
+
+    emp_df = pd.DataFrame(emb_matrix, columns = embedding_names)
+    emp_df['weekday'] = np.arange(0,7)
+
+    return(emp_df)
+
+def calculateTorchEmbeddingMatrix(emb_size, embedding_names, df, batch_size):
+    n_days = 7
+
+    val_idx = get_cv_idxs(len(df))
+    data = ColumnarModelData.from_data_frame('', val_idx, df[['weekday']], df['scaled_users'], ['weekday'], 2)
     
+    def get_emb(num_cat, num_emb):
+        e = nn.Embedding(num_cat, num_emb)
+        e.weight.data.uniform_(-0.01,0.01)
+        return(e)
+
+    class weekdayEmbedding(nn.Module):
+        def __init__(self, n_days):
+            super().__init__()
+            self.weekdays = get_emb(n_days, emb_size)
+            self.lin1 = nn.Linear(emb_size, 40)
+            self.lin2 = nn.Linear(40, 10)
+            self.lin3 = nn.Linear(10,1)
+            #self.drop1 = nn.Dropout(0.5)
+        
+        def forward(self, cats, conts):
+            weekdays = cats[:,0]
+            x = self.weekdays(weekdays)
+            x = F.relu((self.lin1(x)))
+            x = F.relu((self.lin2(x)))
+            return(self.lin3(x))
+
+    model = weekdayEmbedding(n_days).cuda()
+    opt = optim.Adam(model.parameters(), 1e-3)
+    fit(model, data, 30, opt, F.mse_loss)
+    fit(model, data, 30, opt, F.mse_loss)
+
+    emb_matrix =  model.weekdays.weight.data.cpu().numpy()
+
+    emp_df = pd.DataFrame(emb_matrix, columns = embedding_names)
+    emp_df['weekday'] = np.arange(0,7)
+    # list(model.parameters())
+
+    return(emp_df)
+
 
 def plotWeekdayCounts(df, bridge = 'Total'):
     by_day = df.groupby('weekday_name')[[bridge]].sum()
     print(ggplot(by_day.reset_index(), aes('weekday_name', bridge)) + geom_bar(stat = 'identity'))
+
+
+def exploration(df):
+    brigde = 'Brooklyn Bridge'
+    plotWeekdayCounts(df, brigde)
+
+    brigde = 'Williamsburg Bridge'
+    plotWeekdayCounts(df,brigde)
+    ggplot(df, aes(brigde)) + geom_density(aes(fill = 'weekday_name'), alpha = 0.3) + facet_wrap('~weekday_name')
 
